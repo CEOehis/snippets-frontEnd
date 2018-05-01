12621c8c92f46d48b96e71cc8a4d8c5fd3e15c55
ceoehis <ceo.ehis@outlook.com>
2018-05-01 22:36:42 +0100

    add routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import AddNote from './AddNote';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class AppRouting extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar title="Snippet" />
+          <Route exact path="/" component={App} />
+          <Route path="/note/create" component={AddNote} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+ReactDOM.render(<AppRouting />, document.getElementById('root'));
 registerServiceWorker();
 if(module.hot) module.hot.accept();
