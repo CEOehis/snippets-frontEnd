ffcedf692ac2ea08a7354808ceff94d660e7734b
ceoehis <ceo.ehis@outlook.com>
2018-05-01 10:43:37 +0100

    add notes display

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
+import moment from 'moment';
+import shave from 'shave';
+
+global.shave = shave;
+
+class App extends Component {
+  state = {
+    notes: []
+  }
+
+  componentDidMount() {
+    fetch('/api/notes')
+      .then((response => response.json()))
+      .then(result => {
+        this.setState({
+          notes: result.notes
+        })
+        shave('.note', 120)
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Snippet" />
+        <Grid container style={{ padding: 20 }} >
+          <Grid item xs={12}>
+            <Grid container spacing={16}>
+              {this.state.notes.map(note => (
+              <Grid item xs={6} sm={3}key={note._id}>
+                <Card style={{ height: 200 }} >
+                  <CardContent>
+                    <Typography component="h3" style={{fontWeight: 'bold'}} >
+                      {note.title}
+                    </Typography>
+                    <Typography component="small">
+                      {moment(note.updated).format('MMMM Do YYYY')}
+                    </Typography>
+                    <Typography component="p" className="note">
+                      {note.body}
+                    </Typography>
+                  </CardContent>
+
+                </Card>
+              </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default App;
