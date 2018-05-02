9cb1b64a6c349a9bec5bb0bd7b38a731243731dd
ceoehis <ceo.ehis@outlook.com>
2018-05-02 15:07:27 +0100

    stub create note handler

--- src/AddNote.js
+++ src/AddNote.js
@@ -4,9 +4,51 @@ import { FormControl } from 'material-ui/Form'
 import Input, { InputLabel } from 'material-ui/Input';
 import Card from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 
 
 class AddNote extends Component {
+  constructor() {
+    super();
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onBodyChange = this.onBodyChange.bind(this);
+    this.addNote = this.addNote.bind(this);
+    this.state = {
+      title: '',
+      body: ''
+    }
+  }
+
+  onTitleChange(e) {
+    this.setState({
+      title: e.target.value
+    })
+  }
+
+  onBodyChange(e) {
+    this.setState({
+      body: e.target.value
+    })
+  }
+  addNote() {
+    fetch('/api/notes/create', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: this.state.title,
+        body: this.state.body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      console.log(response);
+      return response.json()
+    })
+      .then(data => {
+        console.log(data)
+      });
+  }
 
   render() {
     return (
@@ -19,22 +61,29 @@ class AddNote extends Component {
             <FormControl fullWidth style={{paddingBottom: '20px'}}>
               <InputLabel htmlFor="snippet-title">Title</InputLabel>
               <Input
+                onChange={this.onTitleChange}
                 id="snippet-title"
+                value={this.state.title}
                 inputProps={{
                   'aria-label': 'title',
                 }}
               />
             </FormControl>
-            <FormControl fullWidth>
+            <FormControl fullWidth style={{paddingBottom: '20px'}}>
               <InputLabel htmlFor="snippet-body">Snippet</InputLabel>
               <Input
+                onChange={this.onBodyChange}
                 id="snippet-body"
+                value={this.state.body}
                 multiline
                 inputProps={{
-                  'aria-label': 'snippet',
+                  'aria-label': 'snippet-body',
                 }}
               />
             </FormControl>
+            <Button onClick={this.addNote} variant="raised" color="secondary" aria-label="add">
+              Save
+            </Button>
           </Card>
         </Grid>
       </Grid>
